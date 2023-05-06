e7b749bd1b8dec3801b576a17d0be2998bcc50b0
Danico-Mfala <mfaladan@hotmail.com>
2023-05-07 01:02:53 +0300

    Working on filter section

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,77 @@
+//@ts-nocheck
+
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { Appbar } from 'react-native-paper';
+import React, { useState } from 'react';
+import { Appbar , useTheme,Chip,Button} from 'react-native-paper';
+
+const categories = ["Business", "Health", "Politics", "Sports", "Technology"];
+
+const API_KEY = "pub_217370aa4d8feee12fcb51a2d8bec0dde6d9b";
 
 const Home = () => {
+ 
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const handleSelect = (val: string) => {
+    setSelectedCategories((prev: string[]) =>
+      prev.find((p) => p === val)
+        ? prev.filter((cat) => cat !== val)
+        : [...prev, val]
+    );
+  };
+
+
+
+
+  const handlePress = async () => {
+    const URL = `https://newsdata.io/api/1/news?apikey=${API_KEY}&country=tr&
+    language=en,fr,tr&${selectedCategories.length > 0 ?
+        `&category=${selectedCategories.join()}`
+        : ""
+      }`;
+    try {
+      await fectch(URL)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    } catch (err) {
+      console.log(err);
+      
+    }
+  };
+   
   return (
     <View style={styles.container}>
       <Appbar.Header>
-        <Appbar.Content title="Home"></Appbar.Content>
+        <Appbar.Content title="Home" ></Appbar.Content>
       </Appbar.Header>
+      <View style={styles.filtersContainer}>
+        {categories.map((cat) => (
+          <Chip key={cat} mode="outlined"
+            style={styles.chipItem}
+            textStyle={{
+              fontWeight: "400",
+              color: "white",
+              padding:1
+            }}
+            showSelectedOverlay
+            selected={selectedCategories.find((c) => cat === c) ? true : false}
+            onPress={() => handleSelect(cat)}
+          >
+            {cat}
+          </Chip>
+        ))}
+        <Button
+          mode='outlined'
+          style={styles.button}
+          labelStyle={{
+            fontSize: 14,
+            margin: "auto",
+    
+          }}
+          icon={"sync"}
+          onPress={handlePress}>
+          Refresh
+        </Button>
+      </View>
     </View>
   );
 };
@@ -18,4 +82,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  filtersContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical:10,
+  },
+    chipItem: {
+    marginHorizontal: 5,
+    marginVertical:5,
+  },
+  button:{
+    maxWidth: 400,
+    padding: 0,
+    maxHeight:40,
+  }
 });

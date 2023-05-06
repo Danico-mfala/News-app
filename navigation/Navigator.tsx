5ef0b72b4047bbc53ff2cfaaa2513a8cc7b38ddb
Danico-Mfala <mfaladan@hotmail.com>
2023-05-06 19:45:49 +0300

    working on navigator

--- navigation/Navigator.tsx
+++ navigation/Navigator.tsx
@@ -1,19 +1,36 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import  NewsOverview  from '../screens/NewsOverview';
+import {  Text } from 'react-native';
+import Saved from '../screens/Saved';
+import Home from '../screens/Home';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 function HomeScreen() {
-    return <View>
-        <Text>Home Screen</Text>
-    </View>;
+  return (
+    <Tab.Navigator screenOptions={{headerShown:false}}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Saved" component={Saved} />
+    </Tab.Navigator>
+  );
 }
 
 export default function AppNavigator() {
-    return <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} />
-            <Stack.Screen name="NewsOverview" component={HomeScreen} />
-        </Stack.Navigator>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+              <Stack.Screen
+                  options={{ headerShown: false }}
+                  name="HomeScreen"
+                  component={HomeScreen} />
+              <Stack.Screen
+                  name="NewsOverview"
+                  component={NewsOverview} />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
 }
---
+++ screens/Home.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Appbar } from 'react-native-paper';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Appbar.Header>
+        <Appbar.Content title="Home"></Appbar.Content>
+      </Appbar.Header>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
